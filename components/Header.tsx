8c6e7746fff8bc758ec106d536d6c1b18b2c4f75
Joshua <joshua.edo01@gmail.com>
2023-09-19 13:27:12 +0100

    works

---
+++ components/Header.tsx
@@ -0,0 +1,51 @@
+import Link from "next/link"
+import { Globe, LogIn } from "lucide-react"
+
+import { siteConfig } from "@/config/site"
+import { buttonVariants } from "@/components/ui/Button"
+
+import { ThemeToggle } from "./ThemeToggle"
+
+export function Header() {
+  return (
+    <header className="sticky top-0 z-40 w-full border-b bg-background">
+      <div className="container flex h-16 items-center space-x-4 sm:justify-between sm:space-x-0">
+        {/* TODO: Galleria Logo */}
+
+        {/* TODO: SearchBar */}
+
+        <div className="flex flex-1 items-center justify-end space-x-4">
+          <nav className="flex items-center space-x-1">
+            <Link href={siteConfig.signIn.url} target="_blank" rel="noreferrer">
+              <div
+                className={buttonVariants({
+                  size: "icon",
+                  variant: "ghost",
+                })}
+              >
+                <LogIn className="h-5 w-5" />
+                <span className="sr-only">Sign In</span>
+              </div>
+            </Link>
+            <Link
+              href={siteConfig.links.website}
+              target="_blank"
+              rel="noreferrer"
+            >
+              <div
+                className={buttonVariants({
+                  size: "icon",
+                  variant: "ghost",
+                })}
+              >
+                <Globe className="h-5 w-5 fill-current" />
+                <span className="sr-only">Joshua Edo&apos;s Portfolio</span>
+              </div>
+            </Link>
+            <ThemeToggle />
+          </nav>
+        </div>
+      </div>
+    </header>
+  )
+}
---
+++ config/site.ts
@@ -0,0 +1,20 @@
+export type SiteConfig = typeof siteConfig;
+
+export const siteConfig = {
+  name: " Galleria",
+  description:
+    "Beautifully designed components built with Radix UI and Tailwind CSS.",
+  home: {
+    title: "Home",
+    url: "/"
+  },
+  signIn: {
+    title: "Sign In",
+    url: "/sign-in"
+  },
+  links: {
+    twitter: "https://twitter.com/thegr8joshua",
+    github: "https://github.com/joshuaedo/galleria",
+    website: "https://joshuaedo.com",
+  }
+};
