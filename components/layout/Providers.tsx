e437aa1b0d5d5343f06cbadf290fb19e94dcbcee
Tony <145384399+tonywonatony@users.noreply.github.com>
2023-09-19 23:13:39 +0000

    omo

--- components/layout/Providers.tsx
+++ components/layout/Providers.tsx
@@ -3,19 +3,16 @@
 import * as React from "react"
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
-import { type ThemeProviderProps } from "next-themes/dist/types"
 
 const Providers = ({
   children,
-  ...themeProviderProps
 }: {
   children: React.ReactNode
-  themeProviderProps?: ThemeProviderProps
 }) => {
   const queryClient = new QueryClient()
 
   return (
-    <NextThemesProvider {...themeProviderProps} enableSystem>
+    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
       <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
     </NextThemesProvider>
   )
