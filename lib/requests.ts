2255912697d70644e5b82733b669e83bef27eaea
Tony <145384399+tonywonatony@users.noreply.github.com>
2023-09-20 12:16:50 +0000

    added color

--- lib/requests.ts
+++ lib/requests.ts
@@ -3,30 +3,43 @@ import { toast } from "@/hooks/use-toast"
 
 import { axiosPexelsInstance } from "./axios"
 
+// Function to add color values to photo objects
+function addColorValues(photos, colors) {
+  const photosWithColors = []
+
+  for (let i = 0; i < photos.length; i++) {
+    const photo = photos[i]
+    const color = colors[i % colors.length] // Cycle through colors
+
+    // Create a new photo object with the added color property
+    const photoWithColor = {
+      ...photo,
+      color,
+    }
+
+    photosWithColors.push(photoWithColor)
+  }
+
+  return photosWithColors
+}
+
 export const fetchPhotos = async (): Promise<ImagesResults | undefined> => {
   try {
     const cur = await axiosPexelsInstance.get(`/curated`, {
       params: {
         page: 1,
-        per_page: 40,
+        per_page: 50,
       },
     })
     const curatedResults: ImagesResults = cur.data
 
-    console.log(cur.data)
-
-    const cat = await axiosPexelsInstance.get(`/search`, {
-      params: {
-        page: 1,
-        query: "Cats",
-      },
-    })
-    const searchResults: ImagesResults = cat.data
-
-    console.log(cat.data)
-
     // Parse data with Zod schema
     const parsedData = ImagesSchemaWithPhotos.parse(curatedResults)
+
+    // Add color values to the photos
+    parsedData.photos = addColorValues(parsedData.photos, filterTerms)
+
+    console.log(parsedData)
 
     return parsedData
   } catch (error) {
@@ -34,15 +47,4 @@ export const fetchPhotos = async (): Promise<ImagesResults | undefined> => {
   }
 }
 
-export const searchTerms = [
-  "Cats",
-  "Beach",
-  "Nature",
-  "Sunset",
-  "Food",
-  "Travel",
-  "Flowers",
-  "Cars",
-  "Technology",
-  "Love",
-]
+export const filterTerms = ["Purple", "Green", "Blue", "Yellow", "Red"]
