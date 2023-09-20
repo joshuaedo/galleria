19c27514eade1921995d9fc5bf0001cf3d1c6242
Tony <145384399+tonywonatony@users.noreply.github.com>
2023-09-20 03:44:53 +0000

    why

--- components/Card.tsx
+++ components/Card.tsx
@@ -2,7 +2,14 @@
 
 import { FC } from "react"
 import Image from "next/image"
-import Link from "next/link"
+import { Info } from "lucide-react"
+
+import { Button } from "@/components/ui/Button"
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from "@/components/ui/HoverCard"
 
 interface CardProps {
   alt: string
@@ -13,13 +20,22 @@ const Card: FC<CardProps> = ({ alt, src }) => {
   return (
     <div className="flex justify-center pb-4">
       <div className="relative overflow-hidden">
-        <Image
-          src={src.large}
-          height={1000}
-          width={1000}
-          alt={alt}
-          className="h-[320px] w-[233px] rounded-sm object-cover transition ease-in-out hover:scale-105 md:h-[380px] md:w-[310px]"
-        />
+        <HoverCard>
+          <HoverCardTrigger asChild>
+            <Image
+              src={src.large}
+              height={1000}
+              width={1000}
+              alt={alt}
+              className="h-[320px] w-[233px] rounded-sm object-cover transition ease-in-out hover:scale-105 md:h-[380px] md:w-[310px]"
+            />
+          </HoverCardTrigger>
+          <HoverCardContent className="w-fit p-2">
+            <div className="flex items-center">
+              <span className="text-xs text-muted-foreground">{alt}</span>
+            </div>
+          </HoverCardContent>
+        </HoverCard>
       </div>
     </div>
   )
