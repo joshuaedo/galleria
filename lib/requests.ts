6cfc4872e1298bec7dc3432bfc0d813ff2acda20
Tony <145384399+tonywonatony@users.noreply.github.com>
2023-09-20 13:45:52 +0000

    haew god

--- lib/requests.ts
+++ lib/requests.ts
@@ -1,46 +1,51 @@
-import { ImagesSchemaWithPhotos, ImagesResults } from "@/lib/models";
-import { toast } from "@/hooks/use-toast";
-import { axiosPexelsInstance } from "./axios";
+import { ImagesSchemaWithPhotos, type ImagesResults } from "@/lib/models"
+import { toast } from "@/hooks/use-toast"
+
+import { axiosPexelsInstance } from "./axios"
+import { PexelsPhoto } from '../types/photos';
 
 // Function to add color values to photo objects
-function addColorValues(photos: ImagesResults["photos"], colors: string[]) {
-  const photosWithColors: ImagesResults["photos"] = [];
+function addColorValues(photos, colors) {
+  const photosWithColors = []
 
   for (let i = 0; i < photos.length; i++) {
-    const photo = photos[i];
-    const color = colors[i % colors.length]; // Cycle through colors
+    const photo = photos[i]
+    const color = colors[i % colors.length] // Cycle through colors
 
     // Create a new photo object with the added color property
     const photoWithColor = {
       ...photo,
       color,
-    };
+    }
 
-    photosWithColors.push(photoWithColor);
+    photosWithColors.push(photoWithColor)
   }
 
-  return photosWithColors;
+  return photosWithColors
 }
 
-export const fetchPhotos = async (): Promise<ImagesResults["photos"] | undefined> => {
+export const fetchPhotos = async (): Promise<ImagesResults | undefined> => {
   try {
     const cur = await axiosPexelsInstance.get(`/curated`, {
       params: {
         page: 1,
         per_page: 50,
       },
-    });
-    const curatedResults: ImagesResults = cur.data;
+    })
+    const curatedResults: ImagesResults = cur.data
+
+    // Parse data with Zod schema
+    const parsedData = ImagesSchemaWithPhotos.parse(curatedResults)
 
     // Add color values to the photos
-    curatedResults.photos = addColorValues(curatedResults.photos, filterTerms);
+    parsedData.photos = addColorValues(parsedData.photos, filterTerms)
 
-    console.log(curatedResults);
+    console.log(parsedData)
 
-    return curatedResults.photos;
+    return parsedData as PexelsPhoto[]
   } catch (error) {
-    throw error;
+    throw error
   }
 }
 
-export const filterTerms: string[] = ["purple", "green", "blue", "yellow", "red"];
+export const filterTerms = ["purple", "green", "blue", "yellow", "red"]
--- types/photos.d.ts
+++ types/photos.d.ts
@@ -1,20 +1,43 @@
 export type UnsplashPhoto = {
-  id: number
-  width: number
-  height: number
-  urls: { large: string; regular: string; raw: string; small: string }
-  color: string | null
-  user: {
-    username: string
-    name: string
-  }
-}
+    id: number;
+    width: number;
+    height: number;
+    urls: { large: string; regular: string; raw: string; small: string };
+    color: string | null;
+    user: {
+      username: string;
+      name: string;
+    };
+  };
 
-export type PhotoItem = {
-  alt: string
-  src: {
-    large: string
+  export type PhotoItem = {
+    alt: string
+    src: {
+      large: string
+    }
+    color: string
+    id: number
   }
-  color: string
-  id: number
-}
+
+  export type PexelsPhoto = {
+    id: number;
+    width: number;
+    height: number;
+    url: string;
+    photographer: string;
+    photographer_url: string;
+    photographer_id: number;
+    avg_color: string;
+    src: {
+        original: string;
+        large2x: string;
+        large: string;
+        medium: string;
+        small: string;
+        portrait: string;
+        landscape: string;
+        tiny: string;
+    };
+    liked: boolean;
+    alt: string;
+  }
