0fe61b7fe7720b73a3176a39bd071bddeef37174
Tony <145384399+tonywonatony@users.noreply.github.com>
2023-09-21 15:08:58 +0000

    oi

--- next-auth.d.ts
+++ next-auth.d.ts
@@ -3,11 +3,13 @@ import NextAuth from "next-auth"
 declare module "next-auth" {
   interface User {
     name: string
+    image: string
   }
 
   interface Session {
     user: {
       name: string
+      image: string
     }
   }
 }
