24219ac35d0f86e1a67981fd9dc98fc36079fae1
Tony <145384399+tonywonatony@users.noreply.github.com>
2023-09-21 15:53:56 +0000

    lets try again

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,6 +1,6 @@
 import { auth } from "@/firebase"
 import { signInWithEmailAndPassword } from "firebase/auth"
-import { getServerSession } from "next-auth"
+import { getServerSession } from "next-auth/next"
 import type { NextAuthOptions } from "next-auth/index.d.ts"
 import CredentialsProvider from "next-auth/providers/credentials"
 import GoogleProvider from "next-auth/providers/google"
