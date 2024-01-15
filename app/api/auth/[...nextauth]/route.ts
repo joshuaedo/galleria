a4cb72cb60e44fe542005d5727b304ec7f4d008d
joshuaedo <joshua.edo01@gmail.com>
2024-01-15 15:42:47 +0100

    fix: remove "getAuthSession" as its not a valid Route export field

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,41 +1,38 @@
 import type { NextAuthOptions } from "next-auth/index"
-import NextAuth, { getServerSession } from "next-auth/next"
+import NextAuth from "next-auth/next"
 import GoogleProvider from "next-auth/providers/google"
 
 const authOptions: NextAuthOptions = {
   session: {
-    strategy: 'jwt'
-},
-pages: {
-    signIn: '/'
-},
-providers: [
+    strategy: "jwt",
+  },
+  pages: {
+    signIn: "/",
+  },
+  providers: [
     GoogleProvider({
-        clientId: process.env.GOOGLE_CLIENT_ID!,
-        clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
     }),
-],
-callbacks: {
+  ],
+  callbacks: {
     async session({ token, session }: any) {
-        if (token) {
-            session.user.id = token.id
-            session.user.name = token.name
-            session.user.email = token.email
-            session.user.username = token.username
-            session.user.image = token?.picture
-        }
+      if (token) {
+        session.user.id = token.id
+        session.user.name = token.name
+        session.user.email = token.email
+        session.user.username = token.username
+        session.user.image = token?.picture
+      }
 
-        return session;
+      return session
     },
-    redirect(){
-        return "/"
-    }
-},
-
+    redirect() {
+      return "/"
+    },
+  },
 }
 
 const handler = NextAuth(authOptions)
-
-export const getAuthSession = (req: any, res: any) => getServerSession(req, res, authOptions)
 
 export { handler as GET, handler as POST }
